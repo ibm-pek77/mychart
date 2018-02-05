2129c201ddd29a130c4a3f11884003d83176e49b
wtistang <you@example.com>
2018-02-05 00:23:40 -0800

    all charts added

--- packageAll.sh
+++ packageAll.sh
@@ -6,4 +6,4 @@ helm package traderchart/charts/portfoliochart/
 helm package traderchart/charts/portfoliochart/charts/stockquotechart
 helm package traderchart/charts/portfoliochart/charts/loyaltylevelchart
 mv -f *.tgz docs
-helm repo index docs --url https://ibmcc-pek77.github.io/mychart
+helm repo index docs --url https://ibm-pek77.github.io/mychart
